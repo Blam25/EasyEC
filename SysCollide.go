66f10232d6746335bf76a81bbbb52685bad68ec2
blam25 <adrian.martvall@gmail.com>
2023-04-07 00:58:53 +0200

    nice goroutines

--- SysCollide.go
+++ SysCollide.go
@@ -1,6 +1,9 @@
 package EasyEC
 
-import "image"
+import (
+	"image"
+	"time"
+)
 
 type SysCollide struct {
 }
@@ -12,8 +15,10 @@ func NewSysCollide() {
 
 func (s *SysCollide) Execute() {
 	for _, z := range Components.Collider {
-		z.Collided = false
+
+		//print("yo")
 		s.collide(z)
+
 	}
 }
 func (s *SysCollide) collide(comp *CompCollider) {
@@ -21,13 +26,23 @@ func (s *SysCollide) collide(comp *CompCollider) {
 	rectEnemy := image.Rect(int(G.compRendNPOMap[comp.entity.id].Xpos), int(G.compRendNPOMap[comp.entity.id].Ypos), int(G.compRendNPOMap[comp.entity.id].Xpos)+30, int(G.compRendNPOMap[comp.entity.id].Ypos)+30)
 	rectPlayer := image.Rect(int(Components.Player.Xpos), int(Components.Player.Ypos), int(Components.Player.Xpos)+30, int(Components.Player.Ypos)+30)
 	if rectEnemy.Overlaps(rectPlayer) {
+		if comp.Collided == false {
 
-		comp.Collided = true
-		NewCompCollision(comp.entity, Components.Player.entity)
+			comp.Collided = true
+			go timerThreeSec(comp)
+
+			//comp.Collided = true
+			NewCompCollision(comp.entity, Components.Player.entity)
+		}
 
 		//print("yo whats up")
 		//for _,z := range s.events {
 		//z.execute(s.entity)
 		//}
 	}
+}
+
+func timerThreeSec(comp *CompCollider) {
+	time.Sleep(2 * time.Second)
+	comp.Collided = false
 }
